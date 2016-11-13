0581a7e8f686a1dcfb19242cd1b435454b87968b
Hamza Arab <hamzaarab@cmail.carleton.ca>
2016-11-12 19:16:32 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,19 +2,12 @@
 from tkinter import *
 import tkinter as tk 
 
-global count
 def calculate():
      hours = float(employeeHours.get())
      rate = float(employeeRate.get())   
      taxes = 0.85
      salary = rate * hours * taxes
-     salary2 = salary
-     if count == 0:
-          results = Label(root, text = "Week's Pay: $%.2f" % salary).grid(row=7, column=1)
-     else:
-          salary2, salary = salary, salary2
-          results = Label(root, text = "Week's Pay: $%.2f" % salary2).grid(row=7, column=1)
-          results = Label(root, text = "Last Week's Pay: $%.2f" % salary).grid(row=7, column=1)
+     results = Label(root, text = "Week's Pay: $%.2f" % salary).grid(row=6, column=1)
      return          
 
           
@@ -23,26 +16,25 @@ if __name__ == '__main__':
      root = tk.Tk()
      root.geometry('450x200')
      root.title('Payroll Calculator')
+     can = 0.80
+     us = 0.85
      
-     employeeID = tk.StringVar(root)
      employeeName = tk.StringVar(root)
      employeeHours = tk.StringVar(root)
      employeeRate = tk.StringVar(root)
      
      label1 = Label(root, text = "Welcome to the Payroll Calculator", fg='black').grid(row=0,column=0)  
      
-     label2 = Label(root, text = "EmployeeID: ", fg='red').grid(row=1,column=0)
-     label3 = Label(root, text = "Employee: ", fg='red').grid(row=2,column=0)
-     label4 = Label(root, text = "Enter Hourly Rate: ", fg='red').grid(row=3,column=0)
-     label5 = Label(root, text = "Weekly Hours: ", fg='red').grid(row=4,column=0)
+     label3 = Label(root, text = "Employee: ", fg='red').grid(row=1,column=0)
+     label4 = Label(root, text = "Hourly Rate: ", fg='red').grid(row=2,column=0)
+     label5 = Label(root, text = "Weekly Hours: ", fg='red').grid(row=3,column=0)
       
-     myID = Entry(root, textvariable=employeeID).grid(row=1, column=1)
-     myName = Entry(root, textvariable=employeeName).grid(row=2, column=1)
-     myHours = Entry(root, textvariable=employeeHours).grid(row=4, column=1)
-     myRate = Entry(root, textvariable=employeeRate).grid(row=3, column=1)
+     myName = Entry(root, textvariable=employeeName).grid(row=1, column=1)
+     myHours = Entry(root, textvariable=employeeHours).grid(row=3, column=1)
+     myRate = Entry(root, textvariable=employeeRate).grid(row=2, column=1)
      
      button1 = Button(root, text="Calculate",command = calculate)
-     button1.grid(row=6, column=0)
+     button1.grid(row=5, column=0)
      
      root.mainloop()
                
